0c0b1569df820b9b6b12fe9053c6822c0a87f74b
DivinationHW <107331343+DivinationHW@users.noreply.github.com>
2022-09-10 13:04:26 +0800

    Update mysApi.js

--- plugins/genshin/model/mys/mysApi.js
+++ plugins/genshin/model/mys/mysApi.js
@@ -111,7 +111,7 @@ export default class MysApi {
 
       urlMap.compute.url = `${host}event/e20210225calculate/v2/compute`// 未知
 
-      urlMap.ys_ledger.url = 'https://hk4e-api-os.mihoyo.com/event/ysledgeros/month_info'// 国际服札记
+      urlMap.ys_ledger.url = 'https://hk4e-api-os.mihoyo.com/event/ysledgeros/month_info'// 支持了国际服札记
     }
 
     if (!urlMap[type]) return false
